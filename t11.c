2f24fad83caf4daafaa3d47d4e5da665a0c6396b
albertrein <guilhermealberto8@gmail.com>
2017-08-20 19:50:40 -0300

    Trovado o nome da função

--- t11.c
+++ t11.c
@@ -2,7 +2,7 @@
 #define MAX 10000
 
 
-int faz_a_porra_toda(int contro, int max){
+int faz_tudo(int contro, int max){
 	int i, j, primo ,b, soma=0;
 	int vet[max];
 	printf("#Digite %i números inteiros positivos ou negativos:\n",contro);
@@ -47,58 +47,7 @@ int main(){
 		printf("# Erro. Valor %i inválido\n", controle);
 		printf("ENCERRADO: ERRO\n");
 	}else{
-		faz_a_porra_toda(controle, MAX);
+		faz_tudo(controle, MAX);
 		printf("# FIM\n");
 	}
 }
-
-
-
-
-// C:\> exercT11.exe <ENTER>
-// # Quantos elementos você quer? (1 a 10000)
-// 0<ENTER>
-// # Erro. Valor 0 inválido
-// ENCERRADO: ERRO
-
-// C:\> exercT11.exe <ENTER>
-// # Quantos elementos você quer? (1 a 10000)
-// -5<ENTER>
-// # Erro. Valor -5 inválido
-// ENCERRADO: ERRO
-
-// C:\> exercT11.exe <ENTER>
-// # Quantos elementos você quer? (1 a 10000)
-// 534000<ENTER>
-// # Erro. Valor 534000 inválido
-// ENCERRADO: ERRO
-
-// C:\> exercT11.exe <ENTER>
-// # Quantos elementos você quer? (1 a 10000)
-// 9<ENTER>
-
-// # Digite 9 números inteiros positivos ou negativos:
-// 1199993<ENTER>
-// 0<ENTER>
-// 1<ENTER>
-// 10<ENTER>
-// 55<ENTER>
-// -7<ENTER>
-// 10<ENTER>
-// 2<ENTER>
-// 8921<ENTER>
-
-// # Lido com sucesso 9 inteiros
-//      1199993 IMPAR PRIMO
-//            0 PAR   NAO PRIMO
-//            1 IMPAR NAO PRIMO
-//           10 PAR   NAO PRIMO (DIVIDE POR 2)
-//           55 IMPAR NAO PRIMO (DIVIDE POR 5)
-//           -7 IMPAR PRIMO
-//           10 PAR   NAO PRIMO (DIVIDE POR 2)
-//            2 PAR   PRIMO
-//         8921 IMPAR NAO PRIMO (DIVIDE POR 11)
-
-// SOMA 1208992
-// # FIM
-// C:\> 
