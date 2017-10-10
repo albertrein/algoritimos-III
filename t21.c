d9ad8dbc462f85829aad0cc5454d32fee185311a
Guilherme Reinheimer <guilhermealberto8@gmail.com>
2017-10-10 18:07:05 -0300

    Update t21.c

--- t21.c
+++ t21.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #define STRMAX 501
 #define MAXFUNC 1000
 
@@ -10,11 +9,6 @@ struct FUNC{
 	int n_reg;
 	double salario;
 };
-
- /*Nome do funcionário  (uma string de até STRMAX cars)
-- Endereço residencial (uma string de até STRMAX cars)
-- Número de registro (um inteiro)
-- Salário em reais (double)*/
 
 
 int lestring(char *s, int max)
@@ -36,15 +30,20 @@ int lestring(char *s, int max)
     s[i] = 0;
     return (i);
 }
-
-int safeFflush(){
-	while(fgetc(stdin)!='\n');    
-}
-
 int conta(char *s, int max){
    int i;
    for(i=0;s[i];i++);
    return i;
+}
+
+int letraMaiuscula(char *frases){
+	int contador;
+	for(contador = 0; frases[contador]; contador++){
+		if((frases[contador] >= 'A') && (frases[contador] <= 'Z') || frases[contador] == ' ' || (frases[contador] >= '1' && frases[contador] <= '9' )){
+			continue;
+		}
+		frases[contador] = frases[contador] -32 ;
+	}
 }
 
 struct FUNC leitura_Valores(int qtd, int strmax, double *somatorio){
@@ -53,6 +52,7 @@ struct FUNC leitura_Valores(int qtd, int strmax, double *somatorio){
 
 	printf("#Digite o nome do Funcionario: \n");
 	lestring(xx.nome,strmax);
+	letraMaiuscula(xx.nome);
 	
 	printf("# Endereco\n");
 	lestring(xx.street,strmax);
@@ -79,92 +79,50 @@ struct FUNC leitura_Valores(int qtd, int strmax, double *somatorio){
 
 	return xx;
 }
+int inteiroslidos(){
+    int aux;
+
+    while (scanf("%d", &aux) == 0) {
+        printf("ERRO entrada invalida\n\n");
+        printf("# ERRO. Digitou lixo\n\n");
+        while (fgetc(stdin) != '\n');
+    }
+    return (aux);
+}
+
+int strCMP(char *a, char *b){
+    while (*a && *b && *a == *b) { ++a; ++b; }
+    return (unsigned char)(*a) - (unsigned char)(*b);
+}
 
 int main(int argc, char *argv[]){
 	int i, j, n, aux2, conta;
 	struct FUNC x[MAXFUNC];
 	struct FUNC aux;
 	double soma = 0;
-	char []
-
-	//FALTA REALIZAR A VERIFICAÇÃO DA QUANTIDADE INSERIDO
-
-	// //RARYSON:
-	// if((atoi(argv[1]) <= 0) || (atoi(argv[1]) > MAXFUNC)){
-	// 	if(argv[1] != NULL){
-	// 		printf("ERRO %s\n", argv[1]);
-	// 	}
-	// 	do{
-	// 		printf("#Digite a quantidade de pessoas da sua empresa:\n");
-	// 		conta = scanf("%d",&n);
-	// 		if(conta == 0){
-	// 			printf("ERRO entrada invalida\n");
-	// 			safeFflush();  
-	// 		}
-	// 	} while(n <= 0 || n > MAXFUNC || conta != 1);	
-	// }else{
-	// 	n = atoi(argv[1]);
-	// }
-
-	//EU:
-	if(argc < 2){	//argc não passado por parametro
-		do{ 
-		   	printf("# Digite Quantos Funcionarios Voce Quer Registrar?\n");
-		   	
-		   // 	if (scanf("%i",&n)==0){
-		   // 		printf("ERRO %i\n",n);
-			  //  	n=0;
-		   //    	while(fgetc(stdin)!='\n');
-		   //    	continue;   
-		   // }
-		   if(n < 1 || n > MAXFUNC){
-		   		printf("ERRO %i\n",n);
-			   	n=0;
-		   }
-		}while(n<=0);			
-	}else{	//Recebido argc pela main
-		if(atoi(argv[1])==0){ //RECEBEU NULL
-			printf("ERRO %s\n",argv[1]);
-			do{ 
-			   	printf("# Digite Quantos Funcionarios Voce Quer Registrar?\n");
-			   	if (scanf("%d",&n)==0){
-			      	printf("ERRO ENTRADA\n");
-			      	n = 0;
-			      	while(fgetc(stdin)!='\n');
-			      	continue;   
-			   }
-			   
-			}while(n<=0);
-		}else{
-			n = atoi(argv[1]);
-			
-			do{ 
-				if(n < 1 || n > MAXFUNC){
-				   	printf("ERRO %i\n",n);
-				   	n=0;
-				}
-			   	printf("# Digite Quantos Funcionarios Voce Quer Registrar?\n");
-			   	if (scanf("%d",&n)==0){
-			      	printf("ERRO ENTRADA\n");
-			      	n = 0;
-			      	while(fgetc(stdin)!='\n');
-			      	continue;   
-			   }
-			   
-			}while(n<=0);
-			
-		}	
-	}
-
-	
-
+	if (argc > 1) {
+        n = atoi(argv[1]);
+        if ((n <= 0)||(n >MAXFUNC))  {
+            printf("ERRO %s\n", argv[1]);
+            printf("# ERRO: quantidade de funcionarios deve estar entre 1 e %i\n", MAXFUNC);
+        }
+    }
+    
+    while (n <= 0 || n >MAXFUNC) {
+        printf("# Quantos funcionarios voce quer cadastrar:\n");
+        n = inteiroslidos();
+        if ((n <= 0)||(n >MAXFUNC)) {
+            printf("ERRO %i\n", n);
+            printf("# ERRO: quantidade de funcionarios deve estar entre 1 e %i\n", MAXFUNC);
+        }
+    }
 	for(i=0;i<n;i++){//Leitura de Valores
 		x[i] = leitura_Valores(i,STRMAX,&soma);
 	}
 
 	for (i = 1; i < n; i++) {
 	   for (j=1;j<n;j++) {
-	      if (strcmp(x[j-1].nome, x[j].nome) > 0) {
+	      if (strCMP(x[j-1].nome, x[j].nome) > 0) {
 	         aux = x[j-1];
 	         x[j-1] = x[j];
 	         x[j] = aux;
@@ -172,67 +130,37 @@ int main(int argc, char *argv[]){
 	   }
 	}
 
-	printf("*************************************\n");
+	printf("#*************************************#\n");
 	for(i=0;i<n;i++){//Leitura de Valores
-		printf("Nome: %s\n",x[i].nome);
-		printf("Endereco: %s\n",x[i].street);
-		printf("Registro: %i\n",x[i].n_reg);
-		printf("Salario: %lf\n",x[i].salario);
-		printf("_______________________________\n");
+		printf("%06d %s, %s, R$ %.2lf\n",x[i].n_reg,x[i].nome,x[i].street,x[i].salario);
+		
 	}
-	//FALTA O MODO DE IMPRESSÃO REQUERIDO
-
-	for (i = 1;i<n;i++) {
+	for (i = 0;i<n;i++) {
         for (j = 0;j<n-i;j++) {
             if (x[j].salario < x[j+1].salario) {
                 aux = x[j];
                 x[j] = x[j+1];
                 x[j+1] = aux;
             }
-            soma += x[i].salario;
         }
-    }
-	
-		
+    }	
 
-	printf("*************************************\n");
-	for(i=0;i<n;i++){//Leitura de Valores
-		printf("Nome: %s\n",x[i].nome);
-		printf("Endereco: %s\n",x[i].street);
-		printf("Registro: %i\n",x[i].n_reg);
-		printf("Salario: %lf\n",x[i].salario);
-		printf("_______________________________\n");
+	printf("#*************************************#\n");
+	if(n < 10){
+		for(i = 0; i < n; i++){
+			printf("R$    %.2lf %s\n",x[i].salario,x[i].nome);
+		}
+	
+	}else{
+		for(i = 0; i < 10; i++){
+			printf("R$    %.2lf %s\n",x[i].salario,x[i].nome);
+		}	
 	}
-	//FALTA O MODO DE IMPRESSÃO REQUERIDO2
-    printf("\n SOMATORIO: // %lf\n",soma);
-
-
-
-	/*C:\> exerT21 2<ENTER>
-# Digite os dados para 2 funcionários:
-
-# --- func [0] ---
-# Nome do funcionário:
-Beltronildo fonseca<ENTER>
-
-# Endereço:
-Rua da Universidade, numero primo, Gravatai<ENTER>
-
-# Número do registro
-teste<ENTER>
-ERRO entrada invalida
-# ERRO. Digitou lixo
-
-# Número do registro
-50<ENTER>
-
-# Valor do salário, em reais:
-fdsafsad<ENTER>
-ERRO entrada invalida
-# ERRO. Digitou lixo
-
-# Valor do salário, em reais:
-21128.99<ENTER>*/
-
 	
+	for(i = 0; i < n; i++){
+		soma=soma+x[i].salario;
+	}
+	printf("#Folha de salario total:");
+    printf("\nR$ %.2lf\n",soma);
+    printf("\n# FIM\n");
 }
